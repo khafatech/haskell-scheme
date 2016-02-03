d4a6cb0783ea738214a68e595954c4963e973b81
Mustafa <mushk45@gmail.com>
2016-02-02 20:59:01 -0500

    added LispError data type

--- parse.hs
+++ parse.hs
@@ -1,10 +1,14 @@
 module Main where
 import System.Environment
 import System.IO
-import Text.ParserCombinators.Parsec hiding (spaces)
+
 import Control.Monad
+import Control.Monad.Error
+
 import Numeric
 import Data.Char
+
+import Text.ParserCombinators.Parsec hiding (spaces)
 
 
 
@@ -32,6 +36,31 @@ unwordsList = unwords . map showVal
 -- make instance of the Show type class
 instance Show LispVal where show = showVal
 
+
+data LispError = NumArgs Integer [LispVal]
+               | TypeMismatch String LispVal
+               | Parser ParseError
+               | BadSpecialForm String LispVal
+               | NotFunction String String
+               | UnboundVar String String
+               | Default String
+
+
+showError :: LispError -> String
+showError (UnboundVar message varname)  = message ++ ": " ++ varname
+showError (BadSpecialForm message form) = message ++ ": " ++ show form
+showError (NotFunction message func)    = message ++ ": " ++ show func
+showError (NumArgs expected found)      = "Expected " ++ show expected 
+                                       ++ " args; found values " ++ unwordsList found
+showError (TypeMismatch expected found) = "Invalid type: expected " ++ expected
+                                       ++ ", found " ++ show found
+showError (Parser parseErr)             = "Parse error at " ++ show parseErr
+
+instance Show LispError where show = showError
+
+instance Error LispError where
+    noMsg = Default "An error has occured"
+    strMsg = Default
 
 
 showEither :: Either ParseError LispVal -> String

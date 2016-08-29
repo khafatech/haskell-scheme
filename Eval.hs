a8c637bb132bdb1db04dcb4c2183b599bda0fa14
Mustafa <mushk45@gmail.com>
2016-08-29 09:29:00 -0400

    Added the builtin 'eqv?'

--- Eval.hs
+++ Eval.hs
@@ -68,9 +68,13 @@ primitives = [("+", numericBinopZeroArg (+) 0),
               ("string?", testType "string" . head),
               ("symbol?", testType "symbol" . head),
 
+              ("eq?", eqv),
+              ("eqv?", eqv),
+
               ("car", car),
               ("cdr", cdr),
               ("cons", cons)
+
               ]
 
 
@@ -168,6 +172,26 @@ cons [x, DottedList xs xlast] = return $ DottedList (x : xs) xlast
 cons [x1, x2] = return $ DottedList [x1] x2
 
 cons badArgList = throwError $ NumArgs 2 badArgList
+
+-- eqv ----
+
+eqv :: [LispVal] -> ThrowsError LispVal
+eqv [(Bool arg1), (Bool arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(Number arg1), (Number arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(String arg1), (String arg2)]         = return $ Bool $ arg1 == arg2
+eqv [(Atom arg1), (Atom arg2)]             = return $ Bool $ arg1 == arg2
+eqv [(DottedList xs x), (DottedList ys y)] = eqv [List (xs ++ [x]), List (ys ++ [y])]
+
+-- tests if all elements in the list are `eqv` and of the same length
+eqv [(List arg1), (List arg2)]             = return $ Bool $ (length arg1 == length arg2) &&
+                                                             (all eqvPair $ zip arg1 arg2)
+    where eqvPair (x1, x2) = case eqv [x1, x2] of
+                            Left err -> False
+                            Right (Bool val) -> val
+
+eqv [_, _]                                 = return $ Bool False
+eqv badArgList                             = throwError $ NumArgs 2 badArgList
+
 
 
 
